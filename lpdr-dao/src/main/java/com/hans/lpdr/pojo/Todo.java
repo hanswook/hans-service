b77424aaf1c78c5056663b0d68c39609dab277a0
hanxu <hanxu@wallan-tech.com>
2019-05-11 13:36:13 +0800

    update todo

---
+++ lpdr-dao/src/main/java/com/hans/lpdr/pojo/Todo.java
@@ -0,0 +1,73 @@
+package com.hans.lpdr.pojo;
+
+import java.util.Date;
+import javax.persistence.*;
+
+public class Todo {
+    @Column(name = "u_id")
+    private Integer uId;
+
+    private String content;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    /**
+     * @return u_id
+     */
+    public Integer getuId() {
+        return uId;
+    }
+
+    /**
+     * @param uId
+     */
+    public void setuId(Integer uId) {
+        this.uId = uId;
+    }
+
+    /**
+     * @return content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return create_time
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * @param createTime
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * @return update_time
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * @param updateTime
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
