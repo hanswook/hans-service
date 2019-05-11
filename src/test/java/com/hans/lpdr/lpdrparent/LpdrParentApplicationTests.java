63cd9fa2a8355060dca6cccd3b0790bbacaeb892
hanxu <hanxu@wallan-tech.com>
2019-05-11 11:25:53 +0800

    init

---
+++ src/test/java/com/hans/lpdr/lpdrparent/LpdrParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hans.lpdr.lpdrparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LpdrParentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
